9972869d8f42ce41b20b0a66ae09722e29b0dcce
Adrian Torrie <adriantorrie@gmail.com>
2017-04-02 16:07:56 +1000

    repo cleanup

--- content/downloads/code/eoddata.py
+++ content/downloads/code/eoddata.py
@@ -13,12 +13,30 @@ class Client():
         self._token = self._login(username, password)
         
     def _web_call(self, call, kwargs, pattern=None):
+        """Makes the call to the web sevice.
+        
+        Args:
+         - call    string    Name of the webservice call to make
+         - *kwargs dict      Has the parameter payload (key-value pairs)
+                             the web service call expects. Will typically
+                             require a token (string) to be passed in.
+         - pattern string    The pattern to search for in the returned
+                             XML. When None, this will assume a login
+                             call is being made, as all other responses
+                             will provide an XML response.
+        """
         url = '/'.join((self._web_service, call))
         response = self._session.get(url, params=kwargs, stream=True)
 
+        # 200 is a successfull http response
         if response.status_code == 200:
+            
+            # if no pattern is provided to search for then the raw
+            # response is returned
             if pattern==None:
                 return etree.parse(response.raw).getroot()
+            
+            # search for the pattern and return a pandas DataFrame
             else:
                 root = etree.parse(response.raw).getroot()
                 elements = root.findall(pattern %(self._namespace))
@@ -30,6 +48,7 @@ class Client():
                 
                 return pd.DataFrame(temp)
     
+<<<<<<< Updated upstream
     def _login(self, username, password):
         call = 'Login'
         kwargs = {'Username': username, 'Password': password}
@@ -37,6 +56,40 @@ class Client():
         root = self._web_call(call, kwargs)
         return root.get('Token')
             
+=======
+    def _get_token(self, username=None, password=None):
+        # get credentials from file if none are passed in
+        if username == None and password == None:
+            credentials_file_path = os.path.join(os.path.expanduser('~'), '.eoddata', 'credentials')
+            
+            # test permissions are 0600 (ONLY user can read/write)
+            # do this to ensure the credentials file hasn't been
+            # compromised
+            if (os.path.exists(credentials_file_path) and
+                oct(os.stat(credentials_file_path).st_mode)[-3:] == '600'):
+                try:
+                    with open(credentials_file_path) as f:    
+                        credentials = json.load(f)
+                        username = credentials['username']
+                        password = credentials['password']
+                finally:
+                    del credentials
+        
+        # login to return token 
+        try:
+            call = 'Login'
+            kwargs = {'Username': username, 'Password': password}
+        finally:
+            del username
+            del password
+
+        root = self._web_call(call, kwargs)
+        return root.get('Token')
+    
+    def _get_exchange_codes(self):
+        return list(self.exchange_list()['Code'])
+
+>>>>>>> Stashed changes
     # -------------------------------------------------------------------------
     # external functions
     def close_session():
@@ -53,8 +106,25 @@ class Client():
             
     def get_token(self):
         return self._token
+<<<<<<< Updated upstream
         
     def get_exchange_list(self):
+=======
+    
+    def get_exchange_codes(self):
+        return self._exchange_codes
+      
+    # -------------------------------------------------------------------------
+    # external functions for the client
+    def country_list(self):
+        call = 'CountryList'
+        kwargs = {'Token': self._token,}
+        pattern = ".//{%s}CountryBase"
+        
+        return self._web_call(call, kwargs, pattern)
+    
+    def exchange_list(self):
+>>>>>>> Stashed changes
         call = 'ExchangeList'
         kwargs = {'Token': self._token,}
         pattern = ".//{%s}EXCHANGE"
